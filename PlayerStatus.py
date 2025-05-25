617cc450a7135783e8db1af674ea6bc8be8b33e3
tb-tian <thienan020405@gmail.com>
2025-05-25 09:11:52 +0700

    add outline for player name and score

--- PlayerStatus.py
+++ PlayerStatus.py
@@ -12,6 +12,9 @@ class PlayerStatus():
 		self.playerID = ID
 		self.playerScore = 0
 		self.isAlive = isAlive
+
+		outline_color = Const.BLACK
+		outline_thickness = 2
 		
 		# Avatar
 		self.avaLen = statusLen[1] * 8 / 10
@@ -22,8 +25,16 @@ class PlayerStatus():
 		if ID % 2 != 0:
 			self.skinCoord = (self.statusCoord[0] + self.statusLen[0] - self.statusLen[0] * 1/ 10 - self.avaLen, self.statusCoord[1] + (self.statusLen[1] - self.avaLen) / 2)
 
-		# Player Name
+		# Player Name and Outline
 		playerNameFont = pygame.font.Font('Assets/Fonts/VCR_OSD_MONO.ttf', 25 * self.screenWidth // Const.DELL[0])
+
+		self.playerNameOutlines = []
+		for dx in [-outline_thickness, 0, outline_thickness]:
+			for dy in [-outline_thickness, 0, outline_thickness]:
+				if dx != 0 or dy != 0:
+					outline_text = playerNameFont.render(playerName, True, outline_color)
+					self.playerNameOutlines.append((outline_text, (dx, dy)))
+
 		self.playerNameText = playerNameFont.render(playerName, True, Const.PLAYER_COLOR_DICT[playerColor])
 		playerNameHeight = playerNameFont.size(playerName)[1]
 		playerNameWidth = playerNameFont.size(playerName)[0]
@@ -34,6 +45,14 @@ class PlayerStatus():
 
 		# Score
 		self.playerScoreFont = pygame.font.Font('Assets/Fonts/VCR_OSD_MONO.ttf', 35 * self.screenWidth // Const.DELL[0])
+
+		self.playerScoreOutlines = []
+		for dx in [-outline_thickness, 0, outline_thickness]:
+			for dy in [-outline_thickness, 0, outline_thickness]:
+				if dx != 0 or dy != 0:
+					outline_text = self.playerScoreFont.render(str(self.playerScore), True, outline_color)
+					self.playerScoreOutlines.append((outline_text, (dx, dy)))
+
 		self.playerScoreText = self.playerScoreFont.render(str(self.playerScore), True, Const.PLAYER_COLOR_DICT[playerColor])
 		self.scoreHeight = self.playerScoreFont.size(str(self.playerScore))[1]
 		self.scoreWidth = self.playerScoreFont.size(str(self.playerScore))[0]
@@ -48,12 +67,28 @@ class PlayerStatus():
 		else:
 			self.gameScreen.blit(self.playerDeadAva, self.skinCoord)
 
+		for outline_text, (dx, dy) in self.playerNameOutlines:
+			outline_coord = (self.playerNameCoord[0] + dx, self.playerNameCoord[1] + dy)
+			self.gameScreen.blit(outline_text, outline_coord)
+	
 		self.gameScreen.blit(self.playerNameText, self.playerNameCoord)
 
+		for outline_text, (dx, dy) in self.playerScoreOutlines:
+			outline_coord = (self.playerScoreCoord[0] + dx, self.playerScoreCoord[1] + dy)
+			self.gameScreen.blit(outline_text, outline_coord)
 		self.gameScreen.blit(self.playerScoreText, self.playerScoreCoord)
 
 	def updateScore(self, curScore):
 		self.playerScore = curScore
+		# Regenerate score outlines for new score
+		outline_color = Const.BLACK
+		outline_thickness = 2
+		self.playerScoreOutlines = []
+		for dx in [-outline_thickness, 0, outline_thickness]:
+			for dy in [-outline_thickness, 0, outline_thickness]:
+				if dx != 0 or dy != 0:
+					outline_text = self.playerScoreFont.render(str(self.playerScore), True, outline_color)
+					self.playerScoreOutlines.append((outline_text, (dx, dy)))
 		self.playerScoreText = self.playerScoreFont.render(str(self.playerScore), True, Const.PLAYER_COLOR_DICT[self.playerColor])
 		self.scoreHeight = self.playerScoreFont.size(str(self.playerScore))[1]
 		self.scoreWidth = self.playerScoreFont.size(str(self.playerScore))[0]
